6fc1b717e07b4d9e9316ffb1e43d57e64610a998
HoolioCoolio <hoolio.osuna@gmail.com>
2020-04-22 14:06:09 -0700

    added some different styling

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import React, { useState } from "react";
 import BottomRow from "./BottomRow";
 import "./App.css";
 
-
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
 
   const [homeScore, setHomeScore] = useState(0);
-  const [awayScore, setAwayScore] = useState(0);
+  const [visitorScore, setVisitorScore] = useState(0);
+  // const [downValue, setDownValue] = useState(1);
+  // const [toGoValue, setToGoValue] = useState(1);
+  // const [ballOnValue, setBallOnValue] = useState(50);
+
+
 
 
   return (
@@ -16,15 +20,15 @@ function App() {
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
-            <h2 className="home__name">Lions</h2>
+            <h2 className="home__name">49ERS</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
             <div className="home__score">{homeScore}</div>
           </div>
-          <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{awayScore}</div>
+          <div className="timer">15:00</div>
+          <div className="visitors">
+            <h2 className="visitors__name">RAIDERS</h2>
+            <div className="visitors__score">{visitorScore}</div>
           </div>
         </div>
 
@@ -34,13 +38,17 @@ function App() {
         <div className="homeButtons">
 
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={() => setHomeScore(homeScore + 6)}>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal" onClick={() => setHomeScore(homeScore + 3)}>Home Field Goal</button>
+          <button className="homeButtons__touchdown" onClick={() => setHomeScore(homeScore + 6)}> Home TD </button>
+          <button className="homeButtons__fieldGoal" onClick={() => setHomeScore(homeScore + 3)}> Home FG </button>
         </div>
-        <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={() => setAwayScore(awayScore + 6)}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick={() => setAwayScore(awayScore + 3)}>Away Field Goal</button>
+        <div className="visitorButtons">
+          <button className="visitorButtons__touchdown" onClick={() => setVisitorScore(visitorScore + 6)}> Visitor TD </button>
+          <button className="visitorButtons__fieldGoal" onClick={() => setVisitorScore(visitorScore + 3)}> Visitor FG </button>
         </div>
+        {/* {/* <button className="homeButtons__fieldGoal" onClick={() => setDownValue(downValue + 1)}> Down </button>
+        <button className="homeButtons__fieldGoal" onClick={() => setToGoValue(toGoValue + 1)}> To Go </button>
+        <button className="homeButtons__fieldGoal" onClick={() => setBallOnValue(ballOnValue + 1)}> Ball On </button> */}
+        {/* <button className="homeButtons__fieldGoal" onClick={() => setQuarterValue(quarterValue + 1)}> Quarter </button> */}
       </section>
     </div>
   );
--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,24 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+
+
+
+// const [downValue] = useState(1);
+// const [toGoValue] = useState(1);
+// const [ballOnValue] = useState(1);
+// const [quarterValue, setQuarterValue] = useState(1);
+
+
+
 
 const BottomRow = () => {
   return (
     <div className="bottomRow">
       <div className="down">
         <h3 className="down__title">Down</h3>
-        <div className="down__value">3</div>
+        <div className="down__value">1</div>
+
       </div>
       <div className="toGo">
         <h3 className="toGo__title">To Go</h3>
-        <div className="toGo__value">7</div>
+        <div className="toGo__value">4</div>
+
       </div>
       <div className="ballOn">
-        <h3 className="ballOn__title">Ball on</h3>
-        <div className="ballOn__value">21</div>
+        <h3 className="ballOn__title">Ball On</h3>
+        <div className="ballOn__value">45</div>
+
       </div>
       <div className="quarter">
-        <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
+        <h3 className="quarter__title">QTR</h3>
+        <div className="quarter__value">1</div>
       </div>
     </div>
   );
